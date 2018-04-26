1bf40c0418ca00ee9c7c43e7370f713a95a32ddc
Hasthiya <hardstudio.rambukwella@gmail.com>
2018-04-26 21:44:55 +0530

    Best fit line and simple prediction done

--- myTesting/regression.py
+++ myTesting/regression.py
@@ -1,5 +1,33 @@
-import pandas as pd
-import quandl
+from statistics import mean
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
 
-df = quandl.get('WIKI/GOOGL')
-print(df.head())
+style.use('fivethirtyeight')
+
+xs = np.array([1, 2, 3, 4, 5, 6], dtype=np.float64)
+ys = np.array([5, 2, 4, 5, 6, 7], dtype=np.float64)
+
+
+def best_fit_slope_and_intercept (xs, ys):
+    m = (((mean(xs) * mean(ys)) - mean(xs*ys)) /
+         ((mean(xs)**2) - mean(xs**2)))
+    b = (mean(ys)) - m * mean(xs)
+    return m, b
+
+
+m, b = best_fit_slope_and_intercept(xs, ys)
+
+print(m, b)
+
+regression_line = [(m*x)+b for x in xs]
+
+predict_x = 8
+predict_y = (m*predict_x) + b
+
+plt.scatter(xs, ys)
+plt.scatter(predict_x,predict_y, color='g')
+plt.plot(xs, regression_line)
+plt.show()
+
+
