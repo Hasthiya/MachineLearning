5369958093d479edcfa3c91ee2abecdbd16bdd2c
Hasthiya <hardstudio.rambukwella@gmail.com>
2018-04-29 13:01:08 +0530

    accuracy done

--- myTesting/KNN.py
+++ myTesting/KNN.py
@@ -1,27 +1,52 @@
 import numpy as np
 from math import sqrt
-import matplotlib.pyplot as plt
 import warnings
-from matplotlib import style
 from collections import Counter
-style.use('fivethirtyeight')
-
-# euclidean_distance = sqrt((plot1[0]-plot2[0])**2 + (plot1[1]-plot2[1])**2)
-
-dataset = {'k': [[1, 2], [2, 3], [3, 1]], 'r':[[6, 5], [7, 7], [8, 6]]}
-new_features = [5, 7]
-
-# for i in dataset:
-#     for ii in dataset[i]:
-#         [[plt.scatter(ii[0],ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
-#
-# plt.scatter(new_features[0], new_features[1], s=100)
-# plt.show()
+import pandas as pd
+import random
 
 
 def k_nearest_neighbors(data, predict, k=3):
     if len(data) >= k:
         warnings.warn('K is set to a value less than total voting groups!')
+    distances = []
+    for group in data:
+        for features in data[group]:
+            euclidean_distance = np.linalg.norm(np.array(features)-np.array(predict))
+            distances.append([euclidean_distance, group])
 
-    knnalgos
+    votes = [i[1] for i in sorted(distances)[:k]]
+    # print(Counter(votes).most_common(1))
+    vote_result = Counter(votes).most_common(1)[0][0]
+
     return vote_result
+
+
+df = pd.read_csv('column_3C.dat', sep=None, engine='python')
+df.replace('?', -99999, inplace=True)
+full_data = df.values.tolist()
+random.shuffle(full_data)
+
+test_size = 0.2
+train_set = {'DH': [], 'SL': [], 'NO': []}
+test_set = {'DH': [], 'SL': [], 'NO': []}
+train_data = full_data[:-int(test_size*len(full_data))]
+test_data = full_data[-int(test_size*len(full_data)):]
+
+for i in train_data:
+    train_set[i[-1]].append(i[:-1])
+
+for i in test_data:
+    test_set[i[-1]].append(i[:-1])
+
+correct = 0
+total = 0
+
+for group in test_set:
+    for data in test_set[group]:
+        vote = k_nearest_neighbors(train_set, data, k=5)
+        if group == vote:
+            correct += 1
+        total += 1
+
+print('Accuracy', correct/total)
